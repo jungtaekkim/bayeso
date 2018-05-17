fce1fc793740dd497e4ecd287897334ed620ab76
Jungtaek Kim <jtkim@postech.ac.kr>
2018-05-17 18:11:18 +0900

    UPDATE: test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,10 +1,17 @@
 def test_import_bayeso():
     import bayeso
 
-
 def test_import_bo():
     import bayeso.bo
 
-
 def test_import_gp():
     import bayeso.gp
+
+def test_import_acquisition():
+    import bayeso.acquisition
+
+def test_import_utils():
+    import bayeso.utils
+
+def test_import_benchmarks():
+    import bayeso.benchmarks
