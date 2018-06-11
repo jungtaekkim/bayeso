FIX: covariance test

examples/example_gp.py
@@ -12,7 +12,7 @@ def main():
         [1],
         [2],
     ])
-    Y_train = np.cos(X_train) + np.random.randn(X_train.shape[0], 1) * 0.2
+    Y_train = np.cos(X_train) + np.random.randn(X_train.shape[0], 1) * 0.1
     num_test = 200
     X_test = np.linspace(-3, 3, num_test)
     X_test = X_test.reshape((num_test, 1))

tests/test_covariance.py
@@ -13,13 +13,14 @@ TEST_EPSILON = 1e-5
 
 def test_cov_se():
     with pytest.raises(AssertionError) as error:
-        covariance.cov_se(np.zeros(2), np.zeros(2), 1.0, 0.1)
+        covariance.cov_se(np.zeros(2), np.zeros(2), np.array([1.0, 1.0, 1.0]), 0.1)
     with pytest.raises(AssertionError) as error:
         covariance.cov_se(np.zeros(2), np.zeros(3), np.array([1.0, 1.0]), 0.1)
     with pytest.raises(AssertionError) as error:
         covariance.cov_se(np.zeros(3), np.zeros(2), np.array([1.0, 1.0]), 0.1)
     with pytest.raises(AssertionError) as error:
         covariance.cov_se(np.zeros(2), np.zeros(2), np.array([1.0, 1.0]), 1)
+    assert covariance.cov_se(np.zeros(2), np.zeros(2), 1.0, 0.1) - 0.01 < TEST_EPSILON
 
     bx = np.array([1.0, 2.0, 0.0])
     bxp = np.array([2.0, 1.0, 1.0])