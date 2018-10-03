1af66eed9b44eb9b681c85dc01dbbb94e539a996
Jungtaek Kim <jtkim@postech.ac.kr>
2018-10-04 08:40:57 +0900

    ADD: holder table

--- bayeso/benchmarks.py
+++ bayeso/benchmarks.py
@@ -70,6 +70,21 @@ INFO_HARTMANN6D = {
     'global_minimum_y': -3.32237,
 }
 
+INFO_HOLDERTABLE = {
+    'dim_fun': 2,
+    'bounds': np.array([
+        [-10.0, 10.0],
+        [-10.0, 10.0],
+    ]),
+    'global_minimum_X': np.array([
+        [8.05502, 9.66459],
+        [8.05502, -9.66459],
+        [-8.05502, 9.66459],
+        [-8.05502, -9.66459],
+    ]),
+    'global_minimum_y': -19.2085,
+}
+
 def branin(X,
     a=1.0,
     b=5.1 / (4.0 * np.pi**2),
@@ -169,5 +184,17 @@ def hartmann6d(X):
             outer += alpha[i_] * np.exp(-1.0 * inner)
         Y[ind_, 0] = -1.0 * outer
 
+    return Y
+
+def holdertable(X):
+    assert isinstance(X, np.ndarray)
+    assert len(X.shape) == 1 or len(X.shape) == 2
+    if len(X.shape) == 1:
+        assert X.shape[0] == 2
+        X = np.expand_dims(X, axis=0)
+    elif len(X.shape) == 2:
+        assert X.shape[1] == 2
+
+    Y = -1.0 * np.abs(np.sin(X[:, 0]) * np.cos(X[:, 1]) * np.exp(np.abs(1.0 - np.sqrt(X[:, 0]**2 + X[:, 1]**2) / np.pi)))
     return Y
 
