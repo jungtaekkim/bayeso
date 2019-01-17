FIX: tests for covariance

tests/test_covariance.py
@@ -71,35 +71,34 @@ def test_cov_matern52():
 def test_cov_main():
     cur_hyps = utils_covariance.get_hyps('se', 3)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('se', np.zeros((10, 2)), np.zeros((20, 3)), cur_hyps, 0.001)
+        covariance.cov_main('se', np.zeros((10, 2)), np.zeros((20, 3)), cur_hyps, jitter=0.001)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 2)), cur_hyps, 0.001)
+        covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 2)), cur_hyps, jitter=0.001)
     with pytest.raises(ValueError) as error:
-        covariance.cov_main('se', np.zeros((10, 2)), np.zeros((20, 2)), cur_hyps, 0.001)
+        covariance.cov_main('se', np.zeros((10, 2)), np.zeros((20, 2)), cur_hyps, jitter=0.001)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('se', 1.0, np.zeros((20, 3)), cur_hyps, 0.001)
+        covariance.cov_main('se', 1.0, np.zeros((20, 3)), cur_hyps, jitter=0.001)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('se', np.zeros((10, 2)), 1.0, cur_hyps, 0.001)
+        covariance.cov_main('se', np.zeros((10, 2)), 1.0, cur_hyps, jitter=0.001)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main(1.0, np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, 0.001)
+        covariance.cov_main(1.0, np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, jitter=0.001)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), 2.1, 0.001)
+        covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), 2.1, jitter=0.001)
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, 1)
+        covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, jitter=1)
 
     with pytest.raises(AssertionError) as error:
-        covariance.cov_main('abc', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, 0.001)
+        covariance.cov_main('abc', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, jitter=0.001)
 
     cur_hyps.pop('signal', None)
     with pytest.raises(ValueError) as error:
         covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps)
     cur_hyps = utils_covariance.get_hyps('se', 3)
-    cov_ = covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, 0.001)
+    cov_ = covariance.cov_main('se', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, jitter=0.001)
 
     cur_hyps = utils_covariance.get_hyps('matern32', 3)
-    cov_ = covariance.cov_main('matern32', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, 0.001)
+    cov_ = covariance.cov_main('matern32', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, jitter=0.001)
 
     cur_hyps = utils_covariance.get_hyps('matern52', 3)
-    cov_ = covariance.cov_main('matern52', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, 0.001)
+    cov_ = covariance.cov_main('matern52', np.zeros((10, 3)), np.zeros((20, 3)), cur_hyps, jitter=0.001)
 
-   