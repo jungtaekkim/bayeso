ce869ebc33d1e1b320238db32a0b6eb1aa696867
Jungtaek Kim <jtkim@postech.ac.kr>
2019-11-28 15:59:59 +0900

    UPDATE: update README.md and setup.py

--- setup.py
+++ setup.py
@@ -27,4 +27,13 @@ setup(
     packages=list_packages,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, != 3.3.*, !=3.4.*, <4',
     install_requires=required,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
 )
