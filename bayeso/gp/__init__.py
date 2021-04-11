af7f567d71dd0a7b5670b8ac0da218af398ed42d
Jungtaek Kim <jtkim@postech.ac.kr>
2021-04-11 16:43:21 +0900

    Update docstring

--- bayeso/gp/__init__.py
+++ bayeso/gp/__init__.py
@@ -4,5 +4,6 @@
 #
 """These files are for implementing Gaussian process regression.
 It is implemented, based on the following article:
+
 (i) Rasmussen, C. E., & Williams, C. K. (2006). Gaussian Process
 Regression for Machine Learning. MIT Press."""
