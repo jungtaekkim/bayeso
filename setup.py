482f7066f69e82f867e8438d2c1510088f7de6e2
Jungtaek Kim <jtkim@postech.ac.kr>
2021-10-23 21:55:47 +0900

    Update to version 0.5.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(path_requirements) as f:
 
 setup(
     name='bayeso',
-    version='0.5.1',
+    version='0.5.2',
     author='Jungtaek Kim',
     author_email='jtkim@postech.ac.kr',
     url='https://bayeso.org',
